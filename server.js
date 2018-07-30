3a8a29f40b4bafcb6ab5ef681df562f85a0d66ac
Ben You <byou729@gmail.com>
2018-07-30 18:37:37 -0500

    almost done

--- server.js
+++ server.js
@@ -12,12 +12,12 @@ app.use(bodyParser.json());
 // Basic route that sends the user first to the AJAX Page
 app.get("/", function (req, res) {
     // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "home.html"));
+    res.sendFile(path.join(__dirname, "app/public/home.html"));
 });
 
 app.get("/survey", function (req, res) {
     // res.send("Welcome to the Star Wars Page!")
-    res.sendFile(path.join(__dirname, "survey.html"));
+    res.sendFile(path.join(__dirname, "app/public/survey.html"));
 });
 
 // Create New Characters - takes in JSON input
